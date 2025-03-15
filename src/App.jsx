3eae8866dc7282c398852bb1b4088fda3b1fc4b0
asif2508 <rakibul4210@gmail.com>
2025-03-15 14:28:44 +0600

    react class 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import Profile from "./components/Profile";
 
 function App() {
   // normal variable
@@ -8,41 +7,87 @@ function App() {
   // const name = 'pramod'
 
   // react state
-  const [age, setAge] = useState(50)
+  const [age, setAge] = useState(50);
   const [favSub, setFavSubject] = useState("English");
+  const [success, setSuccess] = useState(false)
+  const [message, setMessage] = useState("")
+
+  // useState =>  manage states
+  // useEffect => manage side effects
+  // useContext => manage global states
+  // useReducer => manage complex states
+  // useMomo => memoizing the data or component
+
+  // useEffect()
+  // takes two params
+  // 1. callback function
+  // 2. dependency array/injection
+
+  // useEffect(() => {
+  //     // all of your logics here
+  //     fetch("https://jsonplaceholder.typicode.com/posts")
+  //       .then((res) => res.json())
+  //       .then((result) => console.log(result));
+  //   },
+  //   [
+  //     // you have to add state or functions or any values here
+  //     // which will help us to re-render the component and re-run the useEffect
+  //   ]
+  // );
 
 
-  const increaseAge = ()=>{
-    setAge(age+1)
-  }
 
-  const decreaseAge = ()=>{
-    setAge(age-1)
-  }
+  useEffect(()=>{
+    if(success){
+      console.log("success")
+      setMessage("The request was successful")
+    }else{
+      console.log("not success")
+      setMessage("The request was not successful")
+    }
+  },[success])
 
+  console.log(success, message)
+
+  const increaseAge = () => {
+    setAge(age + 1);
+  };
+
+  const decreaseAge = () => {
+    setAge(age - 1);
+  };
 
   return (
     <div>
-      <h3>{favSub}</h3>
+      {/* <h3>{favSub}</h3>
       <h3>{age}</h3>
       <button onClick={increaseAge}>increase age</button>
-      <button onClick={decreaseAge}>decrease age</button>
-
+      <button onClick={decreaseAge}>decrease age</button> */}
       {/* props drilling */}
       {/* props drilling is always one way.
       It means you can only send the props value from parent to child
       but not from child to parent. It's also called one way data binding.
 
       */}
-      <Profile age={age} name="jackie chan" roll={21} favSub={favSub} setFavSubject={setFavSubject} />
+      {/* <Profile
+        age={age}
+        name="jackie chan"
+        roll={21}
+        favSub={favSub}
+        setFavSubject={setFavSubject}
+      /> */}
+
+
+
+        {/* conditional rendering */}
+        <h1>{message}</h1>
+        <button onClick={() => setSuccess(true)}>Success</button>
+        <button onClick={() => setSuccess(false)}>Fail</button>
     </div>
   );
 }
 
 export default App;
-
-
-
 
 // file structure react
 // jsx
